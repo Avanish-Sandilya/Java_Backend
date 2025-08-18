8a4259a99a8ce4e16b589ede75a3d61273230255
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-18 07:47:52 +0530

    fib number using recursion

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -6,8 +6,16 @@ class Main{
         return 1;
     }
 
+    public static int fib(int num){
+       if(num==1||num==0){
+           return num;
+       }
+       return fib(num-1)+fib(num-2);
+    }
+
 
     public static void main(String[] args) {
         System.out.println(factorial(5));
+        System.out.println(fib(7));
     }
 }
