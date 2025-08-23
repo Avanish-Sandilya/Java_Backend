f7c26817a30c633bf9b4f1099b17eeb52266e1b9
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-23 18:11:14 +0530

    adding checks

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class BarkingDog {
 
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        return barking && (hourOfDay < 8 || hourOfDay > 22);
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay > 0 && hourOfDay <= 24)
+            return barking && (hourOfDay < 8 || hourOfDay > 22);
+        else
+            return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true,6));
-        System.out.println(shouldWakeUp(true,13));
+        System.out.println(shouldWakeUp(true, 6));
+        System.out.println(shouldWakeUp(true, 13));
     }
 }
