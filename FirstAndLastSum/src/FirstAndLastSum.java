4b36455919f3088e4330887fbf5d817af142d95c
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-24 16:56:17 +0530

    adding FirstAndLastSum Class

---
+++ FirstAndLastSum/src/FirstAndLastSum.java
@@ -0,0 +1,19 @@
+public class FirstAndLastSum {
+
+    public static int sumFirstAndLastDigit(int number){
+        if(number<0){
+            return -1;
+        }
+
+        String num = String.valueOf(number);
+
+        int firstDigit = Character.getNumericValue(num.charAt(0));
+        int lastDigit =  Character.getNumericValue(num.charAt(num.length()-1));
+
+        return firstDigit+lastDigit;
+    }
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(-257));
+    }
+}
