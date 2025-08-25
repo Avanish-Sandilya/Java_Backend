86f37730daa168f0331187dd20e82e5bff6d7e20
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-25 22:36:54 +0530

    adding reformats

--- FirstAndLastSum/src/FirstAndLastSum.java
+++ FirstAndLastSum/src/FirstAndLastSum.java
@@ -1,17 +1,18 @@
 public class FirstAndLastSum {
 
-    public static int sumFirstAndLastDigit(int number){
-        if(number<0){
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
             return -1;
         }
 
         String num = String.valueOf(number);
 
         int firstDigit = Character.getNumericValue(num.charAt(0));
-        int lastDigit =  Character.getNumericValue(num.charAt(num.length()-1));
+        int lastDigit = Character.getNumericValue(num.charAt(num.length() - 1));
 
-        return firstDigit+lastDigit;
+        return firstDigit + lastDigit;
     }
+
     public static void main(String[] args) {
         System.out.println(sumFirstAndLastDigit(257));
         System.out.println(sumFirstAndLastDigit(-257));
