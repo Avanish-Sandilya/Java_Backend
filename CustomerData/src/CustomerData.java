644acae541da8c514b8a747e78506ca5fe42d85c
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-27 22:23:06 +0530

    adding CustomerData class

--- CustomerData/src/CustomerData.java
+++ CustomerData/src/CustomerData.java
@@ -1,2 +1,42 @@
 public class CustomerData {
+    private final String name;
+    private final double creditLimit;
+    private final String email;
+
+    public CustomerData(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public CustomerData(){
+        this("User",200000,"userMail@mailprovider.com");
+    }
+
+    public CustomerData(double creditLimit){
+        this("User",creditLimit,"defaultmail@mailprovider.com");
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerData{" +
+                "name='" + name + '\'' +
+                ", creditLimit=" + creditLimit +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- CustomerData/src/Main.java
+++ CustomerData/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        CustomerData c1= new CustomerData();
+        System.out.println(c1);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
+        CustomerData c2= new CustomerData("Tim",100000,"timb@gmail.com");
+        System.out.println(c2);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
+        CustomerData c3 = new CustomerData(160000);
+        System.out.println(c3);
 
     }
 }
