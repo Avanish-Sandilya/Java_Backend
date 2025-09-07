4dbf774c6e90584d1f172b2373d88bd031400f00
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-07 13:21:33 +0530

    adding reverse sorting book

--- ReverseSort/src/Main.java
+++ ReverseSort/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int [] arr = getArray(10);
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
 
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp=0;
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    public static int[] getArray(int size){
+        int[] arr = new int[size];
+        Random random = new Random();
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]= random.nextInt(100);
+        }
+
+        return arr;
     }
 }
