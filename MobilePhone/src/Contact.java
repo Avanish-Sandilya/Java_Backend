3b426167e5bfea354691b832f58b35585e1a0a83
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-09 20:57:30 +0530

    adding Mobile class

--- MobilePhone/src/Contact.java
+++ MobilePhone/src/Contact.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Contact {
-    private String name;
-    private String phoneNumber;
+    private final String name;
+    private final String phoneNumber;
 
     public String getName() {
         return name;
@@ -15,7 +18,26 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 
-    public Contact createContact(String name,String number){
-        return new Contact(name,number);
+    // Factory method (optional)
+    public static Contact createContact(String name, String number) {
+        return new Contact(name, number);
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contact)) return false;
+        Contact c = (Contact) o;
+        return name.equalsIgnoreCase(c.name) && phoneNumber.equals(c.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name.toLowerCase(), phoneNumber);
     }
 }
---
+++ MobilePhone/src/MobilePhone.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private final String myNumber;
+    private final ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact)) {
+            System.out.println("Contact already exists");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int index = myContacts.indexOf(oldContact);
+        if (index < 0) {
+            System.out.println("Contact not found");
+            return false;
+        }
+        if (findContact(newContact)) {
+            System.out.println("Contact with new details already exists");
+            return false;
+        }
+        myContacts.set(index, newContact);
+        System.out.println("Contact updated");
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        if (myContacts.remove(contact)) {
+            System.out.println("Removal successful");
+            return true;
+        } else {
+            System.out.println("No such contact exists");
+            return false;
+        }
+    }
+
+    public int findContact(String name) {
+        for (int i = 0; i < myContacts.size(); i++) {
+            if (myContacts.get(i).getName().equalsIgnoreCase(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean findContact(Contact contact) {
+        return myContacts.contains(contact);
+    }
+
+    public void printContacts() {
+        if (myContacts.isEmpty()) {
+            System.out.println("No contacts found.");
+            return;
+        }
+        for (int i = 0; i < myContacts.size(); i++) {
+            System.out.println((i + 1) + ". " + myContacts.get(i));
+        }
+    }
+}
