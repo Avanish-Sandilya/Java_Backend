d7e41ca9eb4be808932fd36aca9355743e26f042
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-11 20:44:32 +0530

    adding ValidTriangle check

---
+++ ValidTriangle/src/Solution.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public  static  boolean checkValidity(int a, int b, int c) {
+        // code here
+        return a + b > c && (a + c > b && (b + c >= a));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkValidity(5,4,7));
+    }
+}
