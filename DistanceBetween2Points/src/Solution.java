deec768d737fcb3eb7bf322552dbe32be1373597
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-11 20:39:07 +0530

    adding Solution

---
+++ DistanceBetween2Points/src/Solution.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    public static int distance(int x1, int y1, int x2, int y2) {
+        // Code here
+        return (int)Math.round(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(distance(1,2,3,4));
+    }
+}
