dc2a76bf3a0bdb6ae20a54ba90d3233d1a7b91a4
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-12 23:03:15 +0530

    adding reformats

--- ValidTriangle/src/Solution.java
+++ ValidTriangle/src/Solution.java
@@ -1,11 +1,11 @@
 // User function Template for Java
 class Solution {
-    public  static  boolean checkValidity(int a, int b, int c) {
+    public static boolean checkValidity(int a, int b, int c) {
         // code here
         return a + b > c && (a + c > b && (b + c >= a));
     }
 
     public static void main(String[] args) {
-        System.out.println(checkValidity(5,4,7));
+        System.out.println(checkValidity(5, 4, 7));
     }
 }
