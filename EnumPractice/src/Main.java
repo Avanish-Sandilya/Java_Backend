14cc8883087c92c9bd30fd171995c3da9947f748
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-15 20:30:11 +0530

    adding reformats

--- EnumPractice/src/Main.java
+++ EnumPractice/src/Main.java
@@ -6,17 +6,16 @@ public class Main {
         System.out.println("Enter day of week to know the parking charges");
         System.out.println("1-MON, 2-TUE, 3-WED, 4-THUR, 5-FRI, 6-SAT, 7-SUN");
         String userInput = input.nextLine();
-        System.out.println("Rs: "+getParkingCharge(userInput.toUpperCase())+" per minute");
+        System.out.println("Rs: " + getParkingCharge(userInput.toUpperCase()) + " per minute");
 
     }
 
-    public static double getParkingCharge(String day){
-        try
-        {
+    public static double getParkingCharge(String day) {
+        try {
             DaysOfWeek dayEnum = DaysOfWeek.valueOf(day);
             return dayEnum.getPrice();
-        }catch (Exception e){
-            System.out.println(e+"\nInvalid input");
+        } catch (Exception e) {
+            System.out.println(e + "\nInvalid input");
             return 0;
         }
 
