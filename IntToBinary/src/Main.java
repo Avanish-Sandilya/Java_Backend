b02809739a78cdf5fffad41c14b651ece199511c
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-16 23:48:27 +0530

    adding main code to test conversion

--- IntToBinary/src/Main.java
+++ IntToBinary/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(intToBinary(68));
     }
     public static String intToBinary(int n){
         return Integer.toBinaryString(n);
