68f70a6d17f2988217ec8e7fae87d036a869a24f
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-19 23:38:21 +0530

    adding nthTerm methdod

--- nthTerm/src/Main.java
+++ nthTerm/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(nthTerm(4));
     }
+
+    private static int nthTerm(int n){
+        return n*(n+1)/2;
+    }
+
 }
