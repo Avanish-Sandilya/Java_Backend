80e9e66024808f9474a888ca40a21430a4356650
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-21 20:31:04 +0530

    adding reformats

--- nthTerm/src/Main.java
+++ nthTerm/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(nthTerm(4));
     }
-
     private static int nthTerm(int n){
         return n*(n+1)/2;
     }
