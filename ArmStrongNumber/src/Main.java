7b21cea48f47f1274e7307ff8bbd419a9ec2112c
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-23 21:56:15 +0530

    adding armstrong number method

--- ArmStrongNumber/src/Main.java
+++ ArmStrongNumber/src/Main.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static boolean isArmStrongNumber(int n){
+        char[] str= String.valueOf(n).toCharArray();
+        int sum=0;
+        for(var v:str){
+            sum+= (int) Math.pow(Character.getNumericValue(v),3);
+        }
+        return sum==n;
+    }
 }
