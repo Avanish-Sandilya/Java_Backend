37b5ecc4e79d026bcba6a52fddbf328a8bcfb352
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-23 21:57:02 +0530

    adding test code

--- ArmStrongNumber/src/Main.java
+++ ArmStrongNumber/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(isArmStrongNumber(153));
+        System.out.println(isArmStrongNumber(253));
     }
 
     private static boolean isArmStrongNumber(int n){
