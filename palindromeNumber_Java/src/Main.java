81b39855b616457c78030ca0fd4825aed518a793
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-23 22:10:07 +0530

    adding palidrome check code and test code

--- palindromeNumber_Java/src/Main.java
+++ palindromeNumber_Java/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(isPalindrome(555));
+        System.out.println(isPalindrome(1));
+        System.out.println(isPalindrome(24));
+
+    }
+    private static boolean isPalindrome(int n){
+        String num=String.valueOf(n);
+        int start = 0;
+        int end = num.length()-1;
+
+        while(start<end){
+            if(num.charAt(start)!=num.charAt(end)){
+                return false;
+
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
     }
 }
