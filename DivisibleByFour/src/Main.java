708afe8b79d809e143684f49b3fa31c4fb209777
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-26 14:06:32 +0530

    adding isdivisible method and test code

--- DivisibleByFour/src/Main.java
+++ DivisibleByFour/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(isDivisibleByFour("345345344"));
+    }
 
+    private static int isDivisibleByFour(String N){
+        if (N.length() < 2) {
+            return (Integer.parseInt(N) % 4 == 0) ? 1 : 0;
+        }
+
+        int lastTwoDigits = Integer.parseInt(N.substring(N.length() - 2));
+        return (lastTwoDigits % 4 == 0) ? 1 : 0;
     }
 }
