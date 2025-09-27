7ec8b8622465ce91e8ac29169133bf5d5a542e8a
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-27 12:47:45 +0530

    adding testing code

--- FlyAndTrack/src/Main.java
+++ FlyAndTrack/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        Bird bird= new Bird();
+        bird.move();
+        bird.fly();
+        bird.land();
+        Jet jet = new Jet();
+        jet.takeOff();
+        jet.fly();
+        jet.land();
     }
 }
