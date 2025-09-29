e4d95aec4984a0fd5ca4ae796cc802cae249b6b3
avanish_sandilya <bhanusandilya74@gmail.com>
2025-09-29 22:31:31 +0530

    adding test code in main class

--- ISaveable/src/Main.java
+++ ISaveable/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Player tim = new Player("Tim", 10, 15);
+        System.out.println(tim);
 
+        List<String> savedPlayer = tim.write();
+        Player newPlayer = new Player("Default", 0, 0);
+        newPlayer.read(savedPlayer);
+        System.out.println(newPlayer);
+
+        Monster werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println(werewolf);
     }
 }
