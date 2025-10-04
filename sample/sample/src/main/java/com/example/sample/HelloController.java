1b1f304e39bab70f8eb154858038bd57471f2277
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-04 14:55:03 +0530

    adding Restcontroller

--- sample/sample/src/main/java/com/example/sample/HelloController.java
+++ sample/sample/src/main/java/com/example/sample/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.sample;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
