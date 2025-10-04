3b5256e4472d710a07e3e43ddd7897960949fed9
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-04 15:06:54 +0530

    adding multiply and divide to Calculator Service

--- sample/sample/src/main/java/com/example/sample/CalculatorService.java
+++ sample/sample/src/main/java/com/example/sample/CalculatorService.java
@@ -10,4 +10,12 @@ public class CalculatorService {
     public int subtract(int a,int b){
         return a-b;
     }
+
+    public int multiply(int a,int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
 }
