a1130f454299ed245a90ff379337485d7aca9f06
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-10 21:46:27 +0530

    adding simple reformats

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/Repository/ProfessorRepository.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/Repository/ProfessorRepository.java
@@ -4,4 +4,5 @@ import com.example.UniversityCourseManagement.Entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfessorRepository extends JpaRepository<Professor,Long> {
+
 }
