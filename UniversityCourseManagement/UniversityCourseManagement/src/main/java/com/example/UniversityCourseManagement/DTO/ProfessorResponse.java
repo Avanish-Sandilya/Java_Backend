45f69253e36527feec6ea35ac0c9cc80e60b1f91
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:45:04 +0530

    adding professorResponse DTO code

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/ProfessorResponse.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/ProfessorResponse.java
@@ -1,4 +1,32 @@
 package com.example.UniversityCourseManagement.DTO;
 
 public class ProfessorResponse {
+
+    private final Long id;
+    private final String name;
+    private final String dept;
+    private final String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ProfessorResponse(Long id, String name, String dept, String email) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.email = email;
+    }
 }
