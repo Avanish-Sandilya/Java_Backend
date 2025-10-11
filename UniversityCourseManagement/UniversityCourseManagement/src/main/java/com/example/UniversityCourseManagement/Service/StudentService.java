7ab5cee6af6da1078cadbf125de27b60b1e1b1fa
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 14:22:04 +0530

    adding StudentService class

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/Service/StudentService.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/Service/StudentService.java
@@ -1,14 +1,54 @@
 package com.example.UniversityCourseManagement.Service;
 
+import com.example.UniversityCourseManagement.DTO.StudentResponse;
+import com.example.UniversityCourseManagement.Entity.Student;
 import com.example.UniversityCourseManagement.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class StudentService {
+public class StudentService extends BaseService<StudentResponse> {
     @Autowired
     private StudentRepository studentRepository;
 
 
+    @Override
+    public List<StudentResponse> getAll() {
 
+        List<Student> studentsList = studentRepository.findAll();
+        return studentsList.stream().map(s -> new StudentResponse(s.getId(),s.getName(),s.getAge(),s.getEmail())).toList();
+    }
+
+    @Override
+    public StudentResponse add(StudentResponse entity) {
+        Student s = new Student(entity.getName(),entity.getAge(),entity.getEmail());
+        studentRepository.save(s);
+        return entity;
+    }
+
+    @Override
+    public String delete(Long id) {
+        StudentResponse s= getById(id);
+        if(s.getAge()!=-1){
+            studentRepository.deleteById(id);
+
+            return  "Record deleted:\n" +
+                    "Name: " + s.getId() + "\n" +
+                    "Name: " + s.getName() + "\n";
+        }
+        return "No such record exists";
+    }
+
+    @Override
+    public StudentResponse getById(Long id) {
+        Student student=studentRepository.findById(id).orElse(null);
+        if(student!=null){
+            return new StudentResponse(student.getId(),student.getName(),student.getAge(),student.getEmail());
+        }else{
+            return new StudentResponse(null,null,-1,null);
+        }
+    }
 }
