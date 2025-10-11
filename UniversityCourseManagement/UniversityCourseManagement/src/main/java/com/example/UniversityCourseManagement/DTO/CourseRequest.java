8ac64aedf3206ab8b011aa1b03a0414e10b4224d
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:38:58 +0530

    adding courseRequest DTO code

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/CourseRequest.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/CourseRequest.java
@@ -1,4 +1,34 @@
 package com.example.UniversityCourseManagement.DTO;
 
+import com.example.UniversityCourseManagement.Entity.CREDITS;
+
 public class CourseRequest {
+
+    private String courseName;
+    private String professorName;
+    private Enum<CREDITS> credit;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public Enum<CREDITS> getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Enum<CREDITS> credit) {
+        this.credit = credit;
+    }
 }
