bb6c6140134a148e4e94d962fa6cb10886129ce0
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:49:51 +0530

    adding studentRequest DTO code reformat

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/StudentRequest.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/StudentRequest.java
@@ -2,18 +2,11 @@ package com.example.UniversityCourseManagement.DTO;
 
 public class StudentRequest {
 
-    private Long id;
+
     private String name;
     private int age;
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
