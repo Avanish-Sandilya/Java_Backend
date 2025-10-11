dafcb23d3c85dec1fc3bdd3a42ddbd5df240aa6a
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:43:11 +0530

    adding professorRequest DTO code

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/ProfessorRequest.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/ProfessorRequest.java
@@ -1,4 +1,32 @@
 package com.example.UniversityCourseManagement.DTO;
 
 public class ProfessorRequest {
+
+    private String name;
+    private String dept;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
