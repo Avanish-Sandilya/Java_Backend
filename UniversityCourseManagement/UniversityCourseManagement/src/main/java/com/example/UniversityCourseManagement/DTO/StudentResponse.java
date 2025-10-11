f69a55dbb4527ca7cc9b4c40594f652aaedfd1e1
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:51:41 +0530

    adding studentResponse DTO code

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/StudentResponse.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/StudentResponse.java
@@ -1,4 +1,32 @@
 package com.example.UniversityCourseManagement.DTO;
 
 public class StudentResponse {
+
+    private final Long id;
+    private final String name;
+    private final int age;
+    private final String email;
+
+    public StudentResponse(Long id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
