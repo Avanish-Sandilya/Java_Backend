f3f7d53e534a5d6c53d5286d5ca6e1eb45776b01
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-11 13:41:24 +0530

    adding courseResponse DTO code

--- UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/CourseResponse.java
+++ UniversityCourseManagement/UniversityCourseManagement/src/main/java/com/example/UniversityCourseManagement/DTO/CourseResponse.java
@@ -1,4 +1,34 @@
 package com.example.UniversityCourseManagement.DTO;
 
+import com.example.UniversityCourseManagement.Entity.CREDITS;
+
 public class CourseResponse {
+
+    private final Long id;
+    private final String courseName;
+    private final String professorName;
+    private final Enum<CREDITS> credits;
+
+    public CourseResponse(Long id, String courseName, String professorName, Enum<CREDITS> credits) {
+        this.id = id;
+        this.courseName = courseName;
+        this.professorName = professorName;
+        this.credits = credits;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    public Enum<CREDITS> getCredits() {
+        return credits;
+    }
 }
