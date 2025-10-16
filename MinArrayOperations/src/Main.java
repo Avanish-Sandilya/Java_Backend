d2d2d9a69305e7bbd1fd6fd7b2cdd47460b91d47
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-16 14:31:29 +0530

    adding test code

--- MinArrayOperations/src/Main.java
+++ MinArrayOperations/src/Main.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr = {4, 7, 19, 16};
+        int k = 3;
+        System.out.println(minOps(arr, k));
     }
     static int minOps(int arr[], int k) {
 
