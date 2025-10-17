8745f76d592a24e69f8e6653ccc90922a5b3a4e7
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-17 21:03:52 +0530

    adding test code

--- ArrayReverse/src/Main.java
+++ ArrayReverse/src/Main.java
@@ -4,7 +4,9 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr={1,2,3,4,5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void reverseArray(int[] arr){
