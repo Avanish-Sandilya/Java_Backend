00cfea9296ada75d7e9124aa22219f3eb623ab41
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-07 13:27:19 +0530

    adding code and test code

--- MinCostToMAkeOne/src/Main.java
+++ MinCostToMAkeOne/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        int[] a = {4, 3, 2};
+        int n = a.length;
 
+        System.out.println(cost(a, n));
     }
+
+
+    static int cost(int[] a, int n) {
+        int min = a[0];
+
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min)
+                min = a[i];
+        }
+
+
+        return (n - 1) * min;
+    }
+
+
 }
+
+
