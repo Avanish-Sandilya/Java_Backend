7949dae1dcb187cbfb9ff0b283d199fcc6964ac6
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-09 19:47:31 +0530

    taking user input and adding test code

--- swapTwoIntegers/src/Main.java
+++ swapTwoIntegers/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two numbers separated by comma");
+        String useInput = input.nextLine();
+        String cleanedInput = useInput.trim();
+        int[] arr = new int[2];
+        arr[0]=Integer.parseInt(cleanedInput.substring(0,cleanedInput.indexOf(',')));
+        arr[1]=Integer.parseInt(cleanedInput.substring(cleanedInput.indexOf(',')+1));
+        swap(arr);
+        System.out.println("a: "+arr[0]+", b: "+arr[1]);
     }
 
     public static void swap(int[] a){
