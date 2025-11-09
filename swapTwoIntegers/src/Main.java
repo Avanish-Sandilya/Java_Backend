27ebe757dfdc8ec4ea0dbe29904c5d48e7148cfb
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-09 19:37:02 +0530

    adding swap function to mimic pass by value

---
+++ swapTwoIntegers/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void swap(int[] a){
+        int temp=a[0];
+        a[0]=a[1];
+        a[1]=temp;
+    }
+}
