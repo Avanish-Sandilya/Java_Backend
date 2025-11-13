b3f8c43b18316d23401c913162976c47d2877388
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-13 21:17:39 +0530

    adding person record sorting

--- LambdaFunctions/src/Main.java
+++ LambdaFunctions/src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     record Person(String firstName, String lastName){
@@ -8,6 +13,29 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        List<Person> people = new ArrayList<>(Arrays.asList(
+                new Person("Ram","Prasad"),
+                new Person("Rajendra","Prasad"),
+                new Person("Vallabh","Patel"),
+                new Person("Ramprasad","Bismil")
+        ));
+
+        var comparePeople = new Comparator<Person>(){
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.lastName.compareTo(o2.lastName);
+            }
+        };
+
+        people.sort(comparePeople);
+        System.out.println(people);
+
+        people.sort(((o1, o2) -> o1.firstName.compareTo(o2.firstName)));
+        System.out.println(people);
+
+        people.sort((Comparator.comparing(p ->p.firstName)));
+        System.out.println(people);
 
     }
 }
