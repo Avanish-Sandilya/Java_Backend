83ebb193d7112be01b45b9caaf3cf9ab5c7869af
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-13 21:04:55 +0530

    adding person record

--- LambdaFunctions/src/Main.java
+++ LambdaFunctions/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    record Person(String firstName, String lastName){
+        @Override
+        public String toString(){
+            return firstName+" "+lastName;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
