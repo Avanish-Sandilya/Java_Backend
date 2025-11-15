edc49ed6ef51e6f113571048d573d69b8a1f2bc6
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-15 20:20:17 +0530

    adding print function without biconsumer

--- ConsumerPredicateLambda/src/Main.java
+++ ConsumerPredicateLambda/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        var lengthBreadth = Arrays.asList(new int[]{5,7},new int[]{9,12},new int[]{3,4});
+        lengthBreadth.forEach(s-> System.out.println(Arrays.toString(s)));
     }
 }
