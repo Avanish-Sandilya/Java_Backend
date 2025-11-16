f19981e38bed8741a615747dbeac13a23d734bb7
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-16 11:24:45 +0530

    adding challenge one - print

--- LambdaChallenges/src/Main.java
+++ LambdaChallenges/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
+
+        String line="Its nice and sunny outside";
+
+        Consumer<String> printParts = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                String[] parts = s.split(" ");
+                Arrays.asList(parts).forEach(System.out::println);
+            }
+        };
+
+        printParts.accept(line);
+
+        System.out.println("============================================");
+
+        Consumer<String> printConcise=s->Arrays.asList(s.split(" ")).forEach(System.out::println);
+        printConcise.accept(line);
 
     }
 }
