29fd03df9e9f59bf7a59cc8acb725a47595485e7
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-17 21:07:31 +0530

    adding method references

--- MethodReferenceChallenge/src/Main.java
+++ MethodReferenceChallenge/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>(List.of("Amit","Rajesh","Mili","Shivam","Kriti"));
+
+        names.forEach(System.out::println);
+        System.out.println("=======================");
+        UnaryOperator<String> adding = "Hello "::concat;
+        names.replaceAll(adding);
+        System.out.println(names);
 
     }
 }
