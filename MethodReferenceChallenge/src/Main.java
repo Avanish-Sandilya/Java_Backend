e79ae65fa6a215770c133352f5b20ce88a431b0f
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-17 21:23:33 +0530

    adding method references practice code

--- MethodReferenceChallenge/src/Main.java
+++ MethodReferenceChallenge/src/Main.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Main {
@@ -10,6 +14,21 @@ public class Main {
         System.out.println("=======================");
         UnaryOperator<String> adding = "Hello "::concat;
         names.replaceAll(adding);
+        System.out.println(names);
+        System.out.println("========================");
+        names.replaceAll(name->name.substring(name.indexOf(" ")+1));
+        names.forEach(System.out::println);
+        System.out.println("=========================");
+        ArrayList<String> lastNames = new ArrayList<>(List.of("Singh","Sharma","Sahu","Kushwaha"));
+        Supplier<Integer> nameSupplier = ()->new Random().nextInt(0, lastNames.size());
+        names.replaceAll(name->name+" "+lastNames.get(nameSupplier.get()));
+        System.out.println(names);
+        System.out.println("===========================");
+        Function<String,String> nameReverse = (name->new StringBuilder(name).reverse().toString());
+        names.replaceAll(name->name.concat(" ").concat(nameReverse.apply(name.substring(0,name.indexOf(" ")))));
+        names.forEach(System.out::println);
+        System.out.println("============================");
+        names.replaceAll(String::toUpperCase);
         System.out.println(names);
 
     }
