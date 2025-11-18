4bd927aa0617d307994c3e55f0d72834965d67db
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-18 16:54:47 +0530

    adding andThen

--- ConvinienceMethods/src/Main.java
+++ ConvinienceMethods/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-
+        String name = "Ricky";
+        Function<String,String> surName=s->s.concat(" Ponting");
+        Function<String,String> nameToUpper=String::toUpperCase;
+        Function<String,String> fullName = nameToUpper.andThen(surName);
+        System.out.println(fullName.apply(name));
     }
 }
