920362f82d6075ccd84a843a02a7a55db13564a8
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-18 22:16:48 +0530

    adding comparator chaining example

--- ConvinienceMethods/src/Main.java
+++ ConvinienceMethods/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
 
 public class Main {
@@ -8,5 +11,26 @@ public class Main {
         //Function<String,String> fullName = nameToUpper.andThen(surName);
         Function<String,String> fullName = nameToUpper.compose(surName);
         System.out.println(fullName.apply(name));
+
+        System.out.println("=============================================");
+
+        record person(String firstName,String lastName){}
+
+        ArrayList<person> people = new ArrayList<>(
+                List.of(
+                        new person("Mahendra","Dhoni"),
+                        new person("Ricky","Ponting"),
+                        new person("Virat","Kohli"),
+                        new person("Pat","Cummins"),
+                        new person("Ricky","Rutherford")
+
+                )
+        );
+
+        people.sort(Comparator.comparing(person::firstName).thenComparing(person::lastName));
+        people.forEach(System.out::println);
+
+
+
     }
 }
