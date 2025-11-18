dd3cda8d885ebdc804307adf8024e991eb9968d8
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-18 16:56:42 +0530

    adding compose

--- ConvinienceMethods/src/Main.java
+++ ConvinienceMethods/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         String name = "Ricky";
         Function<String,String> surName=s->s.concat(" Ponting");
         Function<String,String> nameToUpper=String::toUpperCase;
-        Function<String,String> fullName = nameToUpper.andThen(surName);
+        //Function<String,String> fullName = nameToUpper.andThen(surName);
+        Function<String,String> fullName = nameToUpper.compose(surName);
         System.out.println(fullName.apply(name));
     }
 }
