82f9b29d932b2532550264a10bcc53415094ebd3
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-19 22:06:16 +0530

    adding decksupplier method

--- CardGame/src/Main.java
+++ CardGame/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static ArrayList<String> deckSupplier(){
+        ArrayList<String> deck = new ArrayList<>();
+
+        String [] type = {"Diamonds","Spades","Clubs","Hearts"};
+        String[] value = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        for(var v :type){
+            for(var w:value){
+                deck.add(type+" of "+value);
+            }
+        }
+
+        Collections.shuffle(deck);
+
+        return deck;
+
+
+    }
+
 }
