403d8302aa2f7219a4631e2e2c0ecb0de7feba3d
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-26 23:32:20 +0530

    adding cube root code

--- cubeRoot/src/Main.java
+++ cubeRoot/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        double num = 27;
+        double cubeRoot = Math.cbrt(num);
+        System.out.println("Cube root: " + cubeRoot);
     }
 }
