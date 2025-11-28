1799924dcb3718ea9837cb4b83a40cab521479c5
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-28 23:03:59 +0530

    adding reformats

--- cubeRoot/src/Main.java
+++ cubeRoot/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         double num = 27;
         double cubeRoot = Math.cbrt(num);
         System.out.println("Cube root: " + cubeRoot);
+
     }
 }
