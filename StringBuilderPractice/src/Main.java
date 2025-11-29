1c5deaa479a2deffef9008125d862444b07463e1
avanish_sandilya <bhanusandilya74@gmail.com>
2025-11-29 23:27:51 +0530

    adding reverse method

--- StringBuilderPractice/src/Main.java
+++ StringBuilderPractice/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter a string");
+        Scanner input = new Scanner(System.in);
+        String userInput= input.nextLine();
+        StringBuilder s = new StringBuilder(userInput);
+        System.out.println(s.reverse().toString());
     }
 }
