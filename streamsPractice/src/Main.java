164782f5456e044ff41af2df83a0cf1069ef46a7
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-02 16:03:46 +0530

    adding count

--- streamsPractice/src/Main.java
+++ streamsPractice/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -26,6 +27,12 @@ public class Main {
         List<Integer> numList2=List.of(11, 22, 33, 44, 55);
         int sumOfOdds=numList2.stream().filter(num->num%2==1).reduce(0, Integer::sum);
         System.out.println(sumOfOdds);
+
+        System.out.println("===============================================");
+
+        String line = "java streams make java code cleaner and java developers happier";
+        int javaCount = (int)Arrays.stream(line.split(" ")).filter(word->word.equalsIgnoreCase("java")).count();
+        System.out.println(javaCount);
 
     }
 }
