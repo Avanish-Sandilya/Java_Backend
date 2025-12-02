14ecb05fb2e6cbeca3908beb4c4389a67725dd47
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-02 15:27:12 +0530

    adding filter and map warmup

--- streamsPractice/src/Main.java
+++ streamsPractice/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> numList = List.of(1, 5, 7, 2, 10, 3, 12, 19);
+
+        List<Integer> squaredEvens = numList.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * n)
+                .toList();
+
+        squaredEvens.forEach(System.out::println);
 
     }
 }
