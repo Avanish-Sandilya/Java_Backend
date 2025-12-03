baed0384c5da2bfc6131ef216ea5f11a2c2b0e78
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-03 21:03:15 +0530

    adding basic matching

--- RegexPractice/src/Main.java
+++ RegexPractice/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\d+");  // one or more digits
+        Matcher matcher = pattern.matcher("Age: 29");
+
+        if (matcher.find()) {
+            System.out.println(matcher.group()); // 29
+        }
 
     }
 }
