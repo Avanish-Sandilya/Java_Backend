f38e5ee514dc736f875efce15bf836bf0b2255c6
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-04 21:02:46 +0530

    adding reading

--- fileHandlingPractice/src/Main.java
+++ fileHandlingPractice/src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String text = Files.readString(Path.of("C:/Users/91834/Desktop/test.txt"));
+        System.out.println(text);
 
     }
 }
