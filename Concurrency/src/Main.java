cb3ca29a52ee296f7dbd00b924b0bafcf3ef5769
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-07 22:26:58 +0530

    adding Main thread

--- Concurrency/src/Main.java
+++ Concurrency/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        var currentThread = Thread.currentThread();
+        System.out.println(currentThread.getClass().getName());
     }
 }
