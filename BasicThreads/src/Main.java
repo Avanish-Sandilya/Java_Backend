0e6a74c4b119fd22f7ca539e33f46caaeded794c
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-13 15:02:59 +0530

    adding concurrent threads

--- BasicThreads/src/Main.java
+++ BasicThreads/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Main thread running");
+        try {
+            System.out.println("Main thread paused for one second");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        Thread thread = new Thread(()->{
+           String tname = Thread.currentThread().getName();
+            System.out.println(tname+" should take 10 dots to run");
+            for(int i=0;i<10;i++){
+                System.out.print(". ");
+                try{
+                    Thread.sleep(500);
+                }catch (Exception e){
+                    System.out.println(tname+" interrupted");
+                    return;
+                }
+            }
+            System.out.println("\n"+tname+" completed");
+        });
+        System.out.println(thread.getName()+" starting");
+        thread.start();
+
+        System.out.println("Main thread will continue here");
     }
 }
