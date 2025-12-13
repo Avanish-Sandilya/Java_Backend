6dc6290e77f40099e10e0b9dd29563d992e487e6
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-13 16:17:22 +0530

    adding Oddvalues class

---
+++ ThreadPractice/src/OddValues.java
@@ -0,0 +1,16 @@
+public class OddValues extends Thread{
+
+    @Override
+    public void run() {
+        for(int i=1;i<100;i++){
+            if(i%2==1){
+                System.out.println(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
