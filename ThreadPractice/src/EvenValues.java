ce4c60acc9d2cd5f816d88fa75cac28bc083da68
avanish_sandilya <bhanusandilya74@gmail.com>
2025-12-13 16:29:20 +0530

    adding sync

--- ThreadPractice/src/EvenValues.java
+++ ThreadPractice/src/EvenValues.java
@@ -1,7 +1,7 @@
 public class EvenValues implements Runnable{
 
     @Override
-    public void run() {
+    public synchronized void run() {
         for(int i=1;i<100;i++){
             if(i%2==0){
                 System.out.println(i);
--- ThreadPractice/src/OddValues.java
+++ ThreadPractice/src/OddValues.java
@@ -1,7 +1,7 @@
 public class OddValues extends Thread{
 
     @Override
-    public void run() {
+    public synchronized void run() {
         for(int i=1;i<100;i++){
             if(i%2==1){
                 System.out.println(i);
