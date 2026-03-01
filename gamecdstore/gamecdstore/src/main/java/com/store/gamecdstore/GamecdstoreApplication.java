2febaa945bea70cc7d52ffb42d5c4c4c26568ead
avanish_sandilya <bhanusandilya74@gmail.com>
2026-03-01 14:41:54 +0530

    adding boilerplate

---
+++ gamecdstore/gamecdstore/src/main/java/com/store/gamecdstore/GamecdstoreApplication.java
@@ -0,0 +1,13 @@
+package com.store.gamecdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecdstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecdstoreApplication.class, args);
+	}
+
+}
---
+++ gamecdstore/gamecdstore/src/test/java/com/store/gamecdstore/GamecdstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.gamecdstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecdstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
